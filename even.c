03c60e83c166e1af7686d6e5f678eba0a01a129a
tgowri <gowrithulasiraj@gmail.com>
2019-01-25 11:49:26 +0530

    Create even.c

---
+++ even.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int low,high,i;
+clrscr();
+i=low;
+scanf("%d%d",&low,&high);
+while(low<high)
+{
+if(i%2==0)
+{
+printf("%d\t",i);
+}
+i++;
+}
+getch();
+}
